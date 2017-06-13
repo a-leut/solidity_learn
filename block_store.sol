c6ff5204976fc8c1f177e8d08dc751725f5195a2
a-leut <turbomonkeyninja@gmail.com>
2017-06-12 21:48:53 -0600

    Update block_store.sol

--- block_store.sol
+++ block_store.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.11;
 
-/// @title Store a hash value on the block
+/// @title Stores a 64-bit value on the Ethereum blockchain with a timestamp.
 contract BlockStore {
     struct BlockValue {
         uint64 hash_value;
@@ -8,11 +8,25 @@ contract BlockStore {
         address creator;
     }
     
+    // Hash value of node
     BlockValue public record;
     
     function BlockStore(uint64 hash_value) {
         record.hash_value = hash_value;
         record.timestamp = block.timestamp;
         record.creator = msg.sender;
+    }
+    
+    // Getters
+    function getValue() returns(uint64) {
+        return record.hash_value;
+    }
+    
+    function getTimestamp() returns(uint256) {
+        return record.timestamp;
+    }
+    
+    function getCreator() returns(address) {
+        return record.creator;
     }
 }
